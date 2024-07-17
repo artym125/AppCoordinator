b17e6dcd4d6bb24230d34696677b2e3344f4a949
Ostap Artym <otapik125@gmail.com>
2024-07-17 10:03:51 +0300

    Initial Commit

---
+++ AppCoordinator/AppCoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCoordinatorApp.swift
+//  AppCoordinator
+//
+//  Created by Ostap Artym on 17.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppCoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
